d46f679a89596c556b64301450c09e6e3993d522
andradeJVF <joao.felixandrade1201@gmail.com>
2022-07-11 11:08:17 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import NotificationButton from "./components/notificationButton/NotificationButton"
+import Header from "./components/header/Header"
+import SalesCard from "./components/salesCard/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Olá Mundo!</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+
+import './Header.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.linkedin.com/in/joao-andrade-/" target='_blank'> João Andrade</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
